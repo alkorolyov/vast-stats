8f6438ecb2e15b2042509fb6a7230b57c5ae8094
alkorolyov <korol.al@gmail.com>
2024-01-21 05:15:17 +0100

    add update script

--- update.sh
+++ update.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Define ANSI escape codes for colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
 INSTALL_DIR="/opt/vast-stats"
 DATA_DIR="/var/lib/vast-stats"
 
 USER="vast-stats"
 GROUP="vast-stats"
+
+SERVICE_NAME='vast-stats'
 
 if [[ $UID -ne 0 ]]; then
     echo "Installation should be run as root."
@@ -16,8 +24,30 @@ cd /tmp
 git clone https://github.com/alkorolyov/vast-stats/
 cd vast-stats/
 
+echo "=> Stop service"
+systemctl stop $SERVICE_NAME
+
 echo "=> Copy sources to $INSTALL_DIR"
 cp main.py $INSTALL_DIR
 cp -r src $INSTALL_DIR
 chown -R $USER:$GROUP $INSTALL_DIR
 chown -R $USER:$GROUP $DATA_DIR
+
+echo "=> Restart service"
+systemctl start $SERVICE_NAME
+
+sleep 5
+
+# check service status
+status=$(systemctl is-active $SERVICE_NAME)
+if [[ "$status" == "active" ]]; then
+    status="${GREEN}$status${NC}"
+else
+    status="${RED}$status${NC}"
+fi
+echo -e "=> Service status: $status"
+
+echo "=> Remove /tmp files"
+rm -rf /tmp/vast-stats
+
+echo "=> Install complete"
